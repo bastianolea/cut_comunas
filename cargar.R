92b8be3ee309e1b30b78c07e0ff05f999073eed6
bastianolea <bastianolea@gmail.com>
2025-02-20 11:52:13 -0300

    orden geográfico de regiones

--- cargar.R
+++ cargar.R
@@ -1,10 +1,45 @@
-cut_comuna <- readxl::read_xls("datos/datos_originales/CUT_2018_v04.xls") |> 
-  janitor::clean_names() |> 
-  dplyr::rename(codigo_comuna = codigo_comuna_2018) 
+library(dplyr)
+library(janitor)
+library(readxl)
 
-readr::write_csv2(cut_comuna, "cut_comuna.csv")
+# cargar
+cut_comuna_0 <- read_xls("datos/datos_originales/CUT_2018_v04.xls")
 
-# library(pins)
-# board <- board_folder("xxx")
-# board |> pin_write(cut_comuna, "cut_comuna")
-# board |> pin_read("cut_comuna")
+cut_comuna_1 <- cut_comuna_0 |> 
+  clean_names() |> 
+  rename(codigo_comuna = codigo_comuna_2018) 
+
+# corregir comunas
+cut_comuna_2 <- cut_comuna_1 |> 
+  mutate(nombre_comuna = recode(nombre_comuna,
+                                "Los Alamos" = "Los Álamos",
+                                "Los Angeles" = "Los Ángeles",
+                                "Treguaco" = "Trehuaco"))
+
+# guardar versión csv
+readr::write_csv2(cut_comuna_2, "datos/cut_comuna.csv")
+
+# guardar versión R
+cut_comuna_3 <- cut_comuna_2 |> 
+  select(-abreviatura_region, -ends_with("provincia")) |> 
+  mutate(across(starts_with("codigo"), as.numeric)) |> 
+  # ordenar regiones geográficamente
+  mutate(nombre_region = factor(nombre_region,
+                                levels = c("Arica y Parinacota",
+                                           "Tarapacá",
+                                           "Antofagasta",
+                                           "Atacama",
+                                           "Coquimbo",
+                                           "Valparaíso",
+                                           "Metropolitana de Santiago",
+                                           "Libertador General Bernardo O'Higgins",
+                                           "Maule",
+                                           "Ñuble",
+                                           "Biobío",
+                                           "La Araucanía",
+                                           "Los Ríos",
+                                           "Los Lagos",
+                                           "Aysén del General Carlos Ibáñez del Campo",
+                                           "Magallanes y de la Antártica Chilena")))
+
+readr::write_rds(cut_comuna_3, "datos/cut_comuna.rds")
